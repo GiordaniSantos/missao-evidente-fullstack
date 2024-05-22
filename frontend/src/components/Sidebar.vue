b25a58b266b92c621fd29aa06779b5e123995809
Giordani <dani.santossilveira@hotmail.com>
2024-05-21 22:38:16 -0300

    ajustes menu lateral

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="min-w-[160px] w-[160px]  transition-all bg-indigo-700 text-white py-4 px-2">
+  <div class="min-w-[200px] w-[160px]  transition-all bg-indigo-700 text-white py-4 px-2">
     <router-link 
       :to="{name: 'app.dashboard'}" 
       active-class="text-white bg-indigo-600"
       exact-active-class="text-white bg-indigo-600"
       class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
       <span class="mr-2 text-gray-300">
-        <HomeIcon class="w-5"/>
+        <FontAwesomeIcon :icon="faHouse" class="w-5" />
       </span>
       <span class="text-xs">
         Dashboard
       </span>
     </router-link>
-    <router-link :to="{name: 'app.membresias'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+    <router-link 
+      :to="{name: 'app.membresias'}" 
+      active-class="text-white bg-indigo-600"
+      exact-active-class="text-white bg-indigo-600"
+      class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
       <span class="mr-2 text-gray-300">
-        <CalendarIcon class="w-5"/>
+        <FontAwesomeIcon :icon="faCalendarDays" class="w-5" />
       </span>
       <span class="text-xs">
         Frequência
@@ -23,29 +27,28 @@
     <Menu as="div" class="relative inline-block text-left w-full">
       <MenuButton class="flex items-center w-full p-2 rounded transition-colors hover:bg-black/30">
         <span class="mr-2 text-gray-300">
-          <HomeIcon class="w-5"/>
+          <FontAwesomeIcon :icon="faChevronRight" class="w-5" />
         </span>
         <span class="text-xs">
           Ministração
         </span>
-        <ChevronUpDownIcon class="h-5 w-5 text-violet-200 hover:text-violet-100" aria-hidden="true" />
       </MenuButton>
       <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
+      enter-active-class="transition duration-300 ease-out delay-100"
+      enter-from-class="transform opacity-0 -translate-y-1"
+      enter-to-class="transform opacity-100 translate-y-0"
+      leave-active-class="transition duration-200 ease-in"
+      leave-from-class="transform opacity-100 translate-y-0"
+      leave-to-class="transform opacity-0 -translate-y-1"
       >
-        <MenuItems class="right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <MenuItems class="right-0 w-full mt-2 mb-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div class="px-1 py-1">
             <MenuItem v-slot="{ active }">
               <router-link to="{name: 'app.crentes'}" 
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faBook" class="mr-2 h-5 w-5 text-indigo-400" />
                 Estudo
               </router-link>
             </MenuItem>
@@ -54,7 +57,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faUserTie" class="mr-2 h-5 w-5 text-indigo-400" />
                 Sermão
               </router-link>
             </MenuItem>
@@ -63,7 +66,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faBookBible" class="mr-2 h-5 w-5 text-indigo-400" />
                 Estudo Biblico
               </router-link>
             </MenuItem>
@@ -72,7 +75,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faPeopleArrows" class="mr-2 h-5 w-5 text-indigo-400" />
                 Discipulado
               </router-link>
             </MenuItem>
@@ -83,29 +86,28 @@
     <Menu as="div" class="relative inline-block text-left w-full">
       <MenuButton class="flex items-center w-full p-2 rounded transition-colors hover:bg-black/30">
         <span class="mr-2 text-gray-300">
-          <HomeIcon class="w-5"/>
+          <FontAwesomeIcon :icon="faChevronRight" class="w-5" />
         </span>
         <span class="text-xs">
           Ato Pastoral
         </span>
-        <ChevronUpDownIcon class="h-5 w-5 text-violet-200 hover:text-violet-100" aria-hidden="true" />
       </MenuButton>
       <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
+        enter-active-class="transition duration-300 ease-out delay-100"
+        enter-from-class="transform opacity-0 -translate-y-1"
+        enter-to-class="transform opacity-100 translate-y-0"
+        leave-active-class="transition duration-200 ease-in"
+        leave-from-class="transform opacity-100 translate-y-0"
+        leave-to-class="transform opacity-0 -translate-y-1"
       >
-        <MenuItems class="right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <MenuItems class="right-0 w-full mt-2 mb-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div class="px-1 py-1">
             <MenuItem v-slot="{ active }">
               <router-link to="{name: 'app.crentes'}" 
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faChildReaching" class="mr-2 h-5 w-5 text-indigo-400"/>
                 Batismo Infantil
               </router-link>
             </MenuItem>
@@ -114,7 +116,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faPrayingHands" class="mr-2 h-5 w-5 text-indigo-400" />
                 Batismo e Profissão de Fé
               </router-link>
             </MenuItem>
@@ -123,7 +125,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faHandHoldingHeart" class="mr-2 h-5 w-5 text-indigo-400" />
                 Benção Nupcial
               </router-link>
             </MenuItem>
@@ -132,7 +134,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faWineGlassAlt" class="mr-2 h-5 w-5 text-indigo-400" />
                 Santa Ceia
               </router-link>
             </MenuItem>
@@ -143,31 +145,28 @@
     <Menu as="div" class="relative inline-block text-left w-full">
       <MenuButton class="flex items-center w-full p-2 rounded transition-colors hover:bg-black/30">
         <span class="mr-2 text-gray-300">
-          <HomeIcon class="w-5"/>
+          <FontAwesomeIcon :icon="faChevronRight" class="w-5" />
         </span>
         <span class="text-xs">
           Visitação
         </span>
-        <span class="text-right">
-          <ChevronUpDownIcon class="h-5 w-5 text-violet-200 hover:text-violet-100" aria-hidden="true" />
-        </span>
       </MenuButton>
       <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
+        enter-active-class="transition duration-300 ease-out delay-100"
+        enter-from-class="transform opacity-0 -translate-y-1"
+        enter-to-class="transform opacity-100 translate-y-0"
+        leave-active-class="transition duration-200 ease-in"
+        leave-from-class="transform opacity-100 translate-y-0"
+        leave-to-class="transform opacity-0 -translate-y-1"
       >
-        <MenuItems class="right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <MenuItems class="right-0 w-full mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div class="px-1 py-1">
             <MenuItem v-slot="{ active }">
               <router-link :to="{name: 'app.crentes'}" 
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faCross" class="mr-2 h-5 w-5 text-indigo-400" />
                 Crentes
               </router-link>
             </MenuItem>
@@ -176,7 +175,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faHeartBroken" class="mr-2 h-5 w-5 text-indigo-400" />
                 Não Crentes
               </router-link>
             </MenuItem>
@@ -185,7 +184,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faUserLock" class="mr-2 h-5 w-5 text-indigo-400" />
                 Presídios
               </router-link>
             </MenuItem>
@@ -194,7 +193,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faSyringe" class="mr-2 h-5 w-5 text-indigo-400" />
                 Enfermos
               </router-link>
             </MenuItem>
@@ -203,7 +202,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faHospital" class="mr-2 h-5 w-5 text-indigo-400" />
                 Hospitais
               </router-link>
             </MenuItem>
@@ -212,7 +211,7 @@
                 active-class="text-white bg-indigo-600"
                 exact-active-class="text-white bg-indigo-600"
                 :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'group flex w-full items-center rounded-md px-2 py-2 text-sm',]">
-                <AtSymbolIcon :active="active" class="mr-2 h-5 w-5 text-indigo-400" aria-hidden="true"/>
+                <FontAwesomeIcon :icon="faSchool" class="mr-2 h-5 w-5 text-indigo-400" />
                 Escolas
               </router-link>
             </MenuItem>
@@ -224,9 +223,11 @@
 </template>
 
 <script setup>
-  import {HomeIcon, UserGroupIcon, ViewColumnsIcon, AtSymbolIcon, UsersIcon, CalendarIcon, ChartBarIcon, ArrowRightCircleIcon} from '@heroicons/vue/24/outline'
-  import { UserIcon, ChevronUpDownIcon, PowerIcon } from "@heroicons/vue/24/outline"
   import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import { faHouse, faCalendarDays, faBook, faUserTie, faBookBible, faPeopleArrows,
+   faChildReaching, faPrayingHands, faHandHoldingHeart, faWineGlassAlt, faCross, faHeartBroken,
+   faUserLock, faSyringe, faHospital, faSchool, faChevronRight} from '@fortawesome/free-solid-svg-icons'
 </script>
 
 <style scoped>
