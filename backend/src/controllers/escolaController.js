c1b9fc60cec482ef710b3016873991d87d8900d3
GiordaniSantos <dani.santossilveira@hotmail.com>
2024-05-22 19:41:36 -0300

    ajustes escola controller

--- backend/src/controllers/escolaController.js
+++ backend/src/controllers/escolaController.js
@@ -1,50 +1,137 @@
 const express = require('express');
 const { Escola } = require('../models');
+const { Op } = require('sequelize');
+const { pagination, paginationInfo } = require('../helpers/pagination');
+const { formatDate, isIso, checkIsDate } = require('../helpers/date')
 
 const router = express.Router();
 
 router.get('/', async (req, res) =>{
-    const escolas = await Escola.findAll();
-    return res.jsonOK(escolas);
+    const perPage = Math.max(0, parseInt(req.query.per_page || '10', 10));
+    const currentPage = Math.max(0, parseInt(req.query.page || '1', 10));
+    const search = req.query.search || '';
+    const sortField = req.query.sort_field || 'createdAt';
+    const sortDirection = req.query.sort_direction || 'DESC';
+    const offset = (currentPage - 1) * perPage;
+    const userId = req.query.userId;
+
+    const whereCondition = {};
+    if (userId) {
+        whereCondition.userId = userId;
+    }
+    if (search) {
+        whereCondition[Op.and] = [
+            {
+              nome: {
+                [Op.like]: `%${search}%`
+              }
+            },
+            /*{
+                createdAt: {
+                    [Op.eq]: search consulta por data especifica
+                },
+                ou consulta data incicio e data fim
+                {
+                createdAt: {
+                    [Op.gte]: startDate.toISOString(),
+                    [Op.lte]: endDate.toISOString()
+                }
+                }
+            }*/
+          ];
+    }
+    const escolas = await Escola.findAll({
+        where: whereCondition,
+        order: [[sortField, sortDirection]],
+        limit: perPage,
+        offset: offset
+    });
+
+    const count = await Escola.count({ where: whereCondition });
+
+    const linksPagination = pagination('escola', currentPage, count, perPage);
+    const links = paginationInfo('escola', currentPage, count, perPage);
+    
+    return res.json({
+        data: escolas,
+        links,
+        meta: {
+            current_page: currentPage,
+            from: offset + 1,
+            links: linksPagination,
+            last_page: Math.ceil(count / perPage),
+            per_page: perPage,
+            to: offset + escolas.length,
+            total: count
+        }
+    });
 });
 
 router.get('/:id', async (req, res)=>{
     const { id } = req.params;
-    const escola  = await Escola.findOne({where: {id:id}});
+    const userId = req.query.userId;
+
+    const escola  = await Escola.findOne({where: {id:id, userId}});
     if(!escola) return res.jsonNotFound();
-    return res.jsonOK(escola);
+    return res.json(escola);
 });
 
 router.post('/', async(req, res) =>{
-    const {userId} = req.body;
+    const userId = req.query.userId;
 
-    const escola = await Escola.create({userId})
-
-    return res.jsonOK(escola);
+    try {
+        const escola = await Escola.create({userId});
+        return res.jsonOK(escola);
+    } catch (err) {
+        let validateErrors = err.errors;
+        if(validateErrors.length > 0) {
+            let field =  validateErrors[0].path
+            let message =  validateErrors[0].message
+            
+            return res.status(422).json({ field,message })
+        }
+        return res.status(500).json({ message: 'Erro ao criar o registro.' });
+    }
 });
 
 router.put('/:id', async (req, res) =>{
     const { id } = req.params;
+    const userId = req.query.userId;
 
     const { body } = req;
-    const fields = ['userId', 'nome', 'created_at'];
 
-    const escola  = await Escola.findOne({where: {id:id}});
+    const escola  = await Escola.findOne({where: {id:id, userId}});
     if(!escola) return res.jsonNotFound();
+    if(body['createdAt'] && !isIso(body['createdAt'])){
+        if(!checkIsDate(body['createdAt'])) return res.status(422).json({ field: 'createdAt',message: 'valor inválido' })
+        escola.changed('createdAt', true);
+        const isoDate = formatDate(body['createdAt']);
+        escola.set('createdAt', isoDate,{raw: true});
+    }
+    escola.set('nome', body['nome']);
     
-    fields.map(fieldName=>{
-        const newValue = body[fieldName];
-        if(newValue!=undefined) escola[fieldName] = newValue
-    });
+    let validateErrors = await escola.validate({fields: ['nome']}).catch(err => err.errors);
 
-    await escola.save();
+    if(validateErrors.length > 0) {
+        let field =  validateErrors[0].path
+        let message =  validateErrors[0].message
+        
+        return res.status(422).json({ field,message })
+    }
+
+    await escola.save({
+        silent: true,
+        fields: ['nome','createdAt']
+    });
 
     return res.jsonOK(escola);
 });
 
 router.delete('/:id', async (req, res) =>{
     const { id } = req.params;
-    const escola  = await Escola.findOne({where: {id:id}});
+    const userId = req.query.userId;
+
+    const escola  = await Escola.findOne({where: {id:id, userId}});
     if(!escola) return res.jsonNotFound();
     await escola.destroy();
     return res.jsonOK();
